16c97e7167b0c5bd719ce93b1de41728d6b45c45
MinjunShin <mj.irin1260@gmail.com>
2021-07-01 04:43:58 +0900

    Added Changing Brush Size

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
+
 
 canvas.width = 700;
 canvas.height = 700;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -30,11 +34,26 @@ function onMouseMove(event) {
   }
 }
 
+function handeRangeChange (event) {
+  const strokeSize = event.target.value;
+  ctx.lineWidth = strokeSize;  
+}
+
 
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
   //console.log(event.target.style);
+}
+
+function handeModeClick() {
+  if(filling) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "PAINT";
+  }
 }
 
 if (canvas) {
@@ -44,6 +63,14 @@ if (canvas) {
   canvas.addEventListener("mouseleave", stopPainting);
 }
 
+if(range) {
+  range.addEventListener("input", handeRangeChange);
+}
+
+if(mode) {
+  mode.addEventListener("click", handeModeClick);
+}
+
 //console.log(Array.from(colors));
 
-Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick))
+Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
